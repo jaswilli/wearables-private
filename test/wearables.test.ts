922b8f9644c10beb86e2ac2e3fd7f2b77ebef178
Jason Williams <jaswilli@users.noreply.github.com>
2023-07-21 13:37:03 -0600

    Initial commit

---
+++ test/wearables.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as Wearables from '../lib/wearables-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/wearables-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new Wearables.WearablesStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
